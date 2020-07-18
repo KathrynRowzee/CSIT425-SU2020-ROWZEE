d025f00b6d9d5c172ab2d7a14022efae163a2096
Kathryn Rowzee <rowz1123@fredonia.edu>
2020-07-18 15:41:00 -0400

    edit c++ program and add sprint outline

--- hw2-sprint1.cpp
+++ hw2-sprint1.cpp
@@ -2,8 +2,37 @@
 
 using namespace std;
 
-int main(){
-    cout << "testing" << endl;
+const int arraySize = 4;
+
+int main() {
+    int Array1[] = { 4,5,6,7 };
+    int Array2[] = { 2,3,4,5 };
+    int Sum[arraySize + 1];
+
+    for (int i = 0; i < arraySize + 1; i++)
+        Sum[i] = 0;
+
+    bool carry = 0;
+    for (int i = 0; i < arraySize + 1;i++) {
+        if (Array1[i] >= 0 && Array2[i] >= 0)
+            Sum[i] = Array1[i] + Array2[i];
+        if (carry)
+            Sum[i]++;
+        if (Sum[i] > 9) {
+            Sum[i] = Sum[i] - 10;
+            carry = true;
+        }
+        else
+            carry = false;
+    }
+
+    cout << "Sum: ";
+    for (int i = 0; i < arraySize + 1; i++) {
+        cout << Sum[i] << " ";
+    }
+    cout << endl;
+
+
 
     system("pause");
     return 0;
